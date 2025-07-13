a50eb9b80ea596e1f95559f15228417ef30ddb28
Denniskaranja2023! <denniswanyeki2021@gmail.com>
2025-07-14 00:43:56 +0300

    add state and events

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Nav from "./Nav";
-
 import hogs from "../porkers_data";
 
-function App() {
-	return (
-		<div className="App">
+import HogList from "./HogList";
+
+function App() {					
+	return (<div>
 			<Nav />
-		</div>
-	);
+			<HogList hogs={hogs} />
+			</div>)
 }
 
 export default App;
